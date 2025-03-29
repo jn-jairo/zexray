4dcac8c9d40d8f23cc395b8773dd1ad3496ac41f
Jairo Correa <jn.j41r0@gmail.com>
2025-03-29 01:22:41 -0300

    Update raylib

--- lib/zexray/enum/shader_location_index.ex
+++ lib/zexray/enum/shader_location_index.ex
@@ -4,39 +4,40 @@ defmodule Zexray.Enum.ShaderLocationIndex do
 
   ## Values
 
-  | id | name                | description                                                                      |
-  | -- | ------------------- | -------------------------------------------------------------------------------- |
-  |  0 | :vertex_position    | Shader location: vertex attribute: position                                      |
-  |  1 | :vertex_texcoord01  | Shader location: vertex attribute: texcoord01                                    |
-  |  2 | :vertex_texcoord02  | Shader location: vertex attribute: texcoord02                                    |
-  |  3 | :vertex_normal      | Shader location: vertex attribute: normal                                        |
-  |  4 | :vertex_tangent     | Shader location: vertex attribute: tangent                                       |
-  |  5 | :vertex_color       | Shader location: vertex attribute: color                                         |
-  |  6 | :matrix_mvp         | Shader location: matrix uniform: model-view-projection                           |
-  |  7 | :matrix_view        | Shader location: matrix uniform: view (camera transform)                         |
-  |  8 | :matrix_projection  | Shader location: matrix uniform: projection                                      |
-  |  9 | :matrix_model       | Shader location: matrix uniform: model (transform)                               |
-  | 10 | :matrix_normal      | Shader location: matrix uniform: normal                                          |
-  | 11 | :vector_view        | Shader location: vector uniform: view                                            |
-  | 12 | :color_diffuse      | Shader location: vector uniform: diffuse color                                   |
-  | 13 | :color_specular     | Shader location: vector uniform: specular color                                  |
-  | 14 | :color_ambient      | Shader location: vector uniform: ambient color                                   |
-  | 15 | :map_albedo         | Shader location: sampler2d texture: albedo (same as: :map_diffuse)               |
-  | 15 | :map_diffuse        | Shader location: sampler2d texture: diffuse (same as: :map_albedo)               |
-  | 16 | :map_metalness      | Shader location: sampler2d texture: metalness (same as: :map_specular)           |
-  | 16 | :map_specular       | Shader location: sampler2d texture: specular (same as: :map_metalness)           |
-  | 17 | :map_normal         | Shader location: sampler2d texture: normal                                       |
-  | 18 | :map_roughness      | Shader location: sampler2d texture: roughness                                    |
-  | 19 | :map_occlusion      | Shader location: sampler2d texture: occlusion                                    |
-  | 20 | :map_emission       | Shader location: sampler2d texture: emission                                     |
-  | 21 | :map_height         | Shader location: sampler2d texture: height                                       |
-  | 22 | :map_cubemap        | Shader location: samplerCube texture: cubemap                                    |
-  | 23 | :map_irradiance     | Shader location: samplerCube texture: irradiance                                 |
-  | 24 | :map_prefilter      | Shader location: samplerCube texture: prefilter                                  |
-  | 25 | :map_brdf           | Shader location: sampler2d texture: brdf                                         |
-  | 26 | :vertex_boneids     | Shader location: vertex attribute: boneIds                                       |
-  | 27 | :vertex_boneweights | Shader location: vertex attribute: boneWeights                                   |
-  | 28 | :bone_matrices      | Shader location: array of matrices uniform: boneMatrices                         |
+  | id | name                | description                                                            |
+  | -- | ------------------- | ---------------------------------------------------------------------- |
+  |  0 | :vertex_position    | Shader location: vertex attribute: position                            |
+  |  1 | :vertex_texcoord01  | Shader location: vertex attribute: texcoord01                          |
+  |  2 | :vertex_texcoord02  | Shader location: vertex attribute: texcoord02                          |
+  |  3 | :vertex_normal      | Shader location: vertex attribute: normal                              |
+  |  4 | :vertex_tangent     | Shader location: vertex attribute: tangent                             |
+  |  5 | :vertex_color       | Shader location: vertex attribute: color                               |
+  |  6 | :matrix_mvp         | Shader location: matrix uniform: model-view-projection                 |
+  |  7 | :matrix_view        | Shader location: matrix uniform: view (camera transform)               |
+  |  8 | :matrix_projection  | Shader location: matrix uniform: projection                            |
+  |  9 | :matrix_model       | Shader location: matrix uniform: model (transform)                     |
+  | 10 | :matrix_normal      | Shader location: matrix uniform: normal                                |
+  | 11 | :vector_view        | Shader location: vector uniform: view                                  |
+  | 12 | :color_diffuse      | Shader location: vector uniform: diffuse color                         |
+  | 13 | :color_specular     | Shader location: vector uniform: specular color                        |
+  | 14 | :color_ambient      | Shader location: vector uniform: ambient color                         |
+  | 15 | :map_albedo         | Shader location: sampler2d texture: albedo (same as: :map_diffuse)     |
+  | 15 | :map_diffuse        | Shader location: sampler2d texture: diffuse (same as: :map_albedo)     |
+  | 16 | :map_metalness      | Shader location: sampler2d texture: metalness (same as: :map_specular) |
+  | 16 | :map_specular       | Shader location: sampler2d texture: specular (same as: :map_metalness) |
+  | 17 | :map_normal         | Shader location: sampler2d texture: normal                             |
+  | 18 | :map_roughness      | Shader location: sampler2d texture: roughness                          |
+  | 19 | :map_occlusion      | Shader location: sampler2d texture: occlusion                          |
+  | 20 | :map_emission       | Shader location: sampler2d texture: emission                           |
+  | 21 | :map_height         | Shader location: sampler2d texture: height                             |
+  | 22 | :map_cubemap        | Shader location: samplerCube texture: cubemap                          |
+  | 23 | :map_irradiance     | Shader location: samplerCube texture: irradiance                       |
+  | 24 | :map_prefilter      | Shader location: samplerCube texture: prefilter                        |
+  | 25 | :map_brdf           | Shader location: sampler2d texture: brdf                               |
+  | 26 | :vertex_boneids     | Shader location: vertex attribute: boneIds                             |
+  | 27 | :vertex_boneweights | Shader location: vertex attribute: boneWeights                         |
+  | 28 | :bone_matrices      | Shader location: array of matrices uniform: boneMatrices               |
+  | 29 | :vertex_instance_tx | Shader location: vertex attribute: instanceTransform                   |
   """
 
   use Zexray.Enum.EnumBase,
@@ -72,6 +73,7 @@ defmodule Zexray.Enum.ShaderLocationIndex do
       map_brdf: 25,
       vertex_boneids: 26,
       vertex_boneweights: 27,
-      bone_matrices: 28
+      bone_matrices: 28,
+      vertex_instance_tx: 29
     }
 end
--- lib/zexray/enum/shader_uniform_data_type.ex
+++ lib/zexray/enum/shader_uniform_data_type.ex
@@ -4,17 +4,21 @@ defmodule Zexray.Enum.ShaderUniformDataType do
 
   ## Values
 
-  | id | name       | description                         |
-  | -- | ---------- | ----------------------------------- |
-  |  0 | :float     | Shader uniform type: float          |
-  |  1 | :vec2      | Shader uniform type: vec2 (2 float) |
-  |  2 | :vec3      | Shader uniform type: vec3 (3 float) |
-  |  3 | :vec4      | Shader uniform type: vec4 (4 float) |
-  |  4 | :int       | Shader uniform type: int            |
-  |  5 | :ivec2     | Shader uniform type: ivec2 (2 int)  |
-  |  6 | :ivec3     | Shader uniform type: ivec3 (3 int)  |
-  |  7 | :ivec4     | Shader uniform type: ivec4 (4 int)  |
-  |  8 | :sampler2d | Shader uniform type: sampler2d      |
+  | id | name       | description                                  |
+  | -- | ---------- | -------------------------------------------- |
+  |  0 | :float     | Shader uniform type: float                   |
+  |  1 | :vec2      | Shader uniform type: vec2 (2 float)          |
+  |  2 | :vec3      | Shader uniform type: vec3 (3 float)          |
+  |  3 | :vec4      | Shader uniform type: vec4 (4 float)          |
+  |  4 | :int       | Shader uniform type: int                     |
+  |  5 | :ivec2     | Shader uniform type: ivec2 (2 int)           |
+  |  6 | :ivec3     | Shader uniform type: ivec3 (3 int)           |
+  |  7 | :ivec4     | Shader uniform type: ivec4 (4 int)           |
+  |  8 | :uint      | Shader uniform type: unsigned int            |
+  |  9 | :uivec2    | Shader uniform type: uivec2 (2 unsigned int) |
+  | 10 | :uivec3    | Shader uniform type: uivec3 (3 unsigned int) |
+  | 11 | :uivec4    | Shader uniform type: uivec4 (4 unsigned int) |
+  | 12 | :sampler2d | Shader uniform type: sampler2d               |
   """
 
   use Zexray.Enum.EnumBase,
@@ -28,6 +32,10 @@ defmodule Zexray.Enum.ShaderUniformDataType do
       ivec2: 5,
       ivec3: 6,
       ivec4: 7,
-      sampler2d: 8
+      uint: 8,
+      uivec2: 9,
+      uivec3: 10,
+      uivec4: 11,
+      sampler2d: 12
     }
 end
