56933070a7d5d4755d530460c8f39587a2c776dd
Jairo Correa <jn.j41r0@gmail.com>
2025-06-16 23:59:42 -0300

    Code style

--- lib/zexray/cursor.ex
+++ lib/zexray/cursor.ex
@@ -12,36 +12,36 @@ defmodule Zexray.Cursor do
   @doc """
   Shows cursor
   """
-  @spec show_cursor() :: :ok
-  defdelegate show_cursor(), to: NIF, as: :show_cursor
+  @spec show() :: :ok
+  defdelegate show(), to: NIF, as: :show_cursor
 
   @doc """
   Hides cursor
   """
-  @spec hide_cursor() :: :ok
-  defdelegate hide_cursor(), to: NIF, as: :hide_cursor
+  @spec hide() :: :ok
+  defdelegate hide(), to: NIF, as: :hide_cursor
 
   @doc """
   Check if cursor is not visible
   """
-  @spec cursor_hidden?() :: boolean
-  defdelegate cursor_hidden?(), to: NIF, as: :is_cursor_hidden
+  @spec hidden?() :: boolean
+  defdelegate hidden?(), to: NIF, as: :is_cursor_hidden
 
   @doc """
   Enables cursor (unlock cursor)
   """
-  @spec enable_cursor() :: :ok
-  defdelegate enable_cursor(), to: NIF, as: :enable_cursor
+  @spec enable() :: :ok
+  defdelegate enable(), to: NIF, as: :enable_cursor
 
   @doc """
   Disables cursor (lock cursor)
   """
-  @spec disable_cursor() :: :ok
-  defdelegate disable_cursor(), to: NIF, as: :disable_cursor
+  @spec disable() :: :ok
+  defdelegate disable(), to: NIF, as: :disable_cursor
 
   @doc """
   Check if cursor is on the screen
   """
-  @spec cursor_on_screen?() :: boolean
-  defdelegate cursor_on_screen?(), to: NIF, as: :is_cursor_on_screen
+  @spec on_screen?() :: boolean
+  defdelegate on_screen?(), to: NIF, as: :is_cursor_on_screen
 end
